f2c890d55f687ed1c45f03217696bc6280c3fd2d
erikuusitalo <erik@uusitalo.io>
2022-01-28 16:06:11 +0100

    feat: add darkmode

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 export default function Home() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <article className="prose lg:prose-xl">
-        <h1>Hello There 👋</h1>
-        <p>
-          This website (uustalo.io) is under construction. You can come back in
-          the near future; until then.
-        </p>
-        <p>
-          Regards, <br /> Erik Uusitalo
-        </p>
-      </article>
+    <div className="container mx-auto px-4">
+      <div className="w-full h-screen flex justify-center items-center">
+        <article className="prose lg:prose-xl dark:prose-invert">
+          <h1>Hello There 👋</h1>
+          <p>
+            This website (uustalo.io) is under construction. You can come back
+            in the near future; until then.
+          </p>
+          <p>
+            Regards, <br /> Erik Uusitalo
+          </p>
+        </article>
+      </div>
     </div>
   );
 }
